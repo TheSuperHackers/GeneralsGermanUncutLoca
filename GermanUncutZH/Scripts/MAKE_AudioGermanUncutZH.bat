8588af9a85d5f120dde0728e1f4dff9a24a01fd4
xezon <4720891+xezon@users.noreply.github.com>
2021-10-03 10:11:45 +0200

    Improve build order logic of GermanUncutZH audio

--- GermanUncutZH/Scripts/MAKE_AudioGermanUncutZH.bat
+++ GermanUncutZH/Scripts/MAKE_AudioGermanUncutZH.bat
@@ -10,8 +10,10 @@ del /f /q    %GeneratedBigFilesDir%\%BigName%.big
 
 :: Copy .big contents
 xcopy /y %GameFilesOriginalGermanDir%\Data\Audio\Sounds\German\*.*   %GeneratedBigFilesUnpackedDir%\%BigName%\Data\Audio\Sounds\%TargetLanguage%\
-xcopy /y %GameFilesEditedDir%\Data\Audio\Sounds\German\*.*           %GeneratedBigFilesUnpackedDir%\%BigName%\Data\Audio\Sounds\%TargetLanguage%\
+:: Copy clean English audio over muted, missing or muffled German Audio
 xcopy /y %GameFilesOriginalEnglishDir%\Data\Audio\Sounds\English\*.* %GeneratedBigFilesUnpackedDir%\%BigName%\Data\Audio\Sounds\%TargetLanguage%\
+:: Copy all clean German audio over the rest
+xcopy /y %GameFilesEditedDir%\Data\Audio\Sounds\German\*.*           %GeneratedBigFilesUnpackedDir%\%BigName%\Data\Audio\Sounds\%TargetLanguage%\
 
 :: Generate .big file(s)
 %ToolsDir%\GeneralsBigCreator\GeneralsBigCreator.exe -source %GeneratedBigFilesUnpackedDir%\%BigName% -dest %GeneratedBigFilesDir%\%BigName%.big
--- GermanUncutZH/Scripts/MAKE_SpeechGermanUncutZH.bat
+++ GermanUncutZH/Scripts/MAKE_SpeechGermanUncutZH.bat
@@ -10,8 +10,10 @@ del /f /q    %GeneratedBigFilesDir%\%BigName%.big
 
 :: Copy .big contents
 xcopy /y %GameFilesOriginalGermanDir%\Data\Audio\Speech\German\*.*   %GeneratedBigFilesUnpackedDir%\%BigName%\Data\Audio\Speech\%TargetLanguage%\
-xcopy /y %GameFilesEditedDir%\Data\Audio\Speech\German\*.*           %GeneratedBigFilesUnpackedDir%\%BigName%\Data\Audio\Speech\%TargetLanguage%\
+:: Copy clean English audio over muted, missing or muffled German Audio
 xcopy /y %GameFilesOriginalEnglishDir%\Data\Audio\Speech\English\*.* %GeneratedBigFilesUnpackedDir%\%BigName%\Data\Audio\Speech\%TargetLanguage%\
+:: Copy all clean German audio over the rest
+xcopy /y %GameFilesEditedDir%\Data\Audio\Speech\German\*.*           %GeneratedBigFilesUnpackedDir%\%BigName%\Data\Audio\Speech\%TargetLanguage%\
 
 :: Generate .big file(s)
 %ToolsDir%\GeneralsBigCreator\GeneralsBigCreator.exe -source %GeneratedBigFilesUnpackedDir%\%BigName% -dest %GeneratedBigFilesDir%\%BigName%.big
