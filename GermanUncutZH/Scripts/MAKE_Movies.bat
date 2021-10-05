fb6bfea08b938d9af692b868190c1fd33055b5b2
xezon <4720891+xezon@users.noreply.github.com>
2021-10-05 23:22:28 +0200

    Fix minor issue in MAKE_Movies.bat script

--- GermanUncutZH/Scripts/MAKE_Movies.bat
+++ GermanUncutZH/Scripts/MAKE_Movies.bat
@@ -2,7 +2,7 @@ set ThisDir1="%~dp0."
 call %ThisDir1%\SETUP_Folders.bat
 
 :: Free folders of files
-del /f /q %GeneratedReleaseUnpackedDir%\Data\German\Movies\*.*
+del /f /q %GeneratedReleaseUnpackedDir%\Data\%TargetLanguage%\Movies\*.*
 
 :: Copy files
 xcopy /y %GameFilesOriginalGermanDir%\Data\German\Movies\*.* %GeneratedReleaseUnpackedDir%\Data\%TargetLanguage%\Movies\
